880e4911e6f23a65b87ac21703efda2477a263d7
raryosu <me@raryosu.info>
2017-06-27 18:50:51 +0900

    :innocent: Difficult to write
    - どうすりゃいいんじゃ

--- sources/k-means/generate_samples.py
+++ sources/k-means/generate_samples.py
@@ -20,10 +20,16 @@ initial_centroids = choose_random_centroids(samples, n_clusters)
 nearest_indices = assign_to_nearest(samples, initial_centroids)
 updated_centroids = update_centroids(samples, nearest_indices, n_clusters)
 
+# Update
+# 多分ここでupdateというTensorを作って
+# assign_to_nearestとupdate_centoirdsをするように定義してやればいい？
+update = 0
+
 model = tf.global_variables_initializer()
 with tf.Session() as session:
     sample_values = session.run(samples)
-    updated_centroid_value = session.run(updated_centroids)
-    print(updated_centroid_value)
+    for i in range(10):
+        updated_centroid_value = session.run(update)
+        print(updated_centroid_value)
 
-plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
+        plot_clusters(sample_values, updated_centroid_value, n_samples_per_cluster)
