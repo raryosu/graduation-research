1dffc9980d638343e14dbbc63e6a5dd373668842
raryosu <me@raryosu.info>
2017-07-16 14:49:56 +0900

    [WIP] 相変わらずBICの計算がうまくいかない
    - 対数尤度関数が怪しい気がする

--- sources/x-means/functions.py
+++ sources/x-means/functions.py
@@ -15,6 +15,80 @@ import numpy as np
 from scipy import stats
 from sklearn.cluster import KMeans
 
+class XMeans:
+    """
+    x-means法を行うクラス
+    """
+
+    def __init__(self, k_init = 2, **k_means_args):
+        self.k_init = k_init
+        self.k_means_args = k_means_args
+
+    def fit(self, X):
+        self.__clusters = [] 
+
+        clusters = self.Cluster.build(X, KMeans(self.k_init, **self.k_means_args).fit(X))
+        self.__recursively_split(clusters)
+
+        self.labels_ = np.empty(X.shape[0], dtype = np.intp)
+        for i, c in enumerate(self.__clusters):
+            self.labels_[c.index] = i
+
+        self.cluster_centers_ = np.array([c.center for c in self.__clusters])
+        self.cluster_log_likelihoods_ = np.array([c.log_likelihood() for c in self.__clusters])
+        self.cluster_sizes_ = np.array([c.size for c in self.__clusters])
+
+        return self
+
+    def __recursively_split(self, clusters):
+        for cluster in clusters:
+            if cluster.size <= 3:
+                self.__clusters.append(cluster)
+                continue
+
+            k_means = KMeans(2, **self.k_means_args).fit(cluster.data)
+            c1, c2 = self.Cluster.build(cluster.data, k_means, cluster.index)
+            print(c1.data)
+            print(c2.data)
+
+            # Den Pelleg (2000)
+            bic = (c1.log_likelihood() + c2.log_likelihood()) - cluster.df/2 * np.log(cluster.size)
+            # Ishioka (2000)
+            # beta = np.linalg.norm(c1.center - c2.center) / np.sqrt(np.linalg.det(c1.cov) + np.linalg.det(c2.cov))
+            # alpha = 0.5 / stats.norm.cdf(beta)
+            # bic = (cluster.size * np.log(alpha) + c1.log_likelihood() + c2.log_likelihood()) - cluster.df/2 * np.log(cluster.size)
+
+            if bic > cluster.bic():
+                self.__recursively_split([c1, c2])
+            else:
+                self.__clusters.append(cluster)
+
+    class Cluster:
+        @classmethod
+        def build(cls, X, k_means, index = None):
+            if np.any(index == None):
+                index = np.array(range(0, X.shape[0]))
+            labels = range(0, k_means.get_params()["n_clusters"])
+
+            return tuple(cls(X, index, k_means, label) for label in labels)
+
+        def __init__(self, X, index, k_means, label):
+            self.data = X[k_means.labels_ == label]
+            self.index = index[k_means.labels_ == label]
+            self.size = self.data.shape[0]
+            self.df = self.data.shape[1]
+            self.center = k_means.cluster_centers_[label]
+            self.cov = np.cov(self.data.T)
+            # 分散を対角行列に
+            # self.cov = np.diag(np.linalg.norm(np.array(self.data - np.mean(self.data, axis=0)), axis=0) ** 2)
+
+        def log_likelihood(self):
+            return sum(stats.multivariate_normal.logpdf(x, self.center, self.cov) for x in self.data)
+            # return sum((x.size * (np.log(2 * np.pi)/2 - self.df/2 * np.log(np.linalg.det(self.cov))) - (x.size - self.center.size) / 2) for x in self.data)
+
+        def bic(self):
+            return self.log_likelihood() - self.df/2 * np.log(self.size)
+
 def plot_samples(all_samples, save=False, name='before'):
     import matplotlib
     matplotlib.use('Agg')
@@ -24,28 +98,24 @@ def plot_samples(all_samples, save=False, name='before'):
     if save:
         plt.savefig("img/{}.pdf".format(name))
 
-def plot_clusters(all_samples, labels, centroids, n_samples_per_cluster, save=True, name='after'):
-    """
-    Plot the clusters with different colour for each cluster
-    """
+def plot_clusters(all_samples, labels, n_samples_per_cluster, centroids, edit_centroids=True, save=True, name='after'):
     import matplotlib
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
-    colour = plt.cm.rainbow(np.linspace(0,1,len(centroids)))
+    colour = plt.cm.rainbow(np.linspace(0, 1, len(centroids)))
     for i, centroid in enumerate(centroids):
         samples = np.array([data for j, data in enumerate(all_samples) if labels[j]==i])
         plt.scatter(samples[:,0], samples[:,1], c=colour[i])
-        plt.plot(centroid[0], centroid[1], markersize=35, marker="x", color='k', mew=10)
-        plt.plot(centroid[0], centroid[1], markersize=30, marker="x", color='m', mew=5)
+        if edit_centroids:
+            plt.plot(centroid[0], centroid[1], markersize=35, marker="x", color='k', mew=10)
+            plt.plot(centroid[0], centroid[1], markersize=30, marker="x", color='m', mew=5)
     plt.show()
     if save:
         plt.savefig("img/{}.pdf".format(name))
 
+
+# 以下 使ってない関数群
 def choose_random_centroids(samples, n_clusters, seed=None):
-    """
-    Initialisation
-    - Select `n_clusters` numboer of random points
-    """
     n_samples = tf.shape(samples)[0]
     random_indices = tf.random_shuffle(tf.range(0, n_samples), seed=seed)
     begin = [0,]
@@ -98,93 +168,6 @@ def k_means(samples, centroids, n_clusters, threshold):
         if len(np.where(np.array( diff.eval().flatten() <= threshold ) )[0]) == n_clusters*2:
             break
     return (samples, nearest_indices, centroids)
-
-class XMeans:
-    """
-    x-means法を行うクラス
-    """
-
-    def __init__(self, k_init = 2, **k_means_args):
-        """
-        k_init : The initial number of clusters applied to KMeans()
-        """
-        self.k_init = k_init
-        self.k_means_args = k_means_args
-
-    def fit(self, X):
-        """
-        x-means法を使ってデータXをクラスタリングする
-        X : array-like or sparse matrix, shape=(n_samples, n_features)
-        """
-        self.__clusters = [] 
-
-        clusters = self.Cluster.build(X, KMeans(self.k_init, **self.k_means_args).fit(X))
-        self.__recursively_split(clusters)
-
-        self.labels_ = np.empty(X.shape[0], dtype = np.intp)
-        for i, c in enumerate(self.__clusters):
-            self.labels_[c.index] = i
-
-        self.cluster_centers_ = np.array([c.center for c in self.__clusters])
-        self.cluster_log_likelihoods_ = np.array([c.log_likelihood() for c in self.__clusters])
-        self.cluster_sizes_ = np.array([c.size for c in self.__clusters])
-
-        return self
-
-    def __recursively_split(self, clusters):
-        """
-        引数のclustersを再帰的に分割する
-        clusters : list-like object, which contains instances of 'XMeans.Cluster'
-        """
-        for cluster in clusters:
-            if cluster.size <= 3:
-                self.__clusters.append(cluster)
-                continue
-
-            k_means = KMeans(2, **self.k_means_args).fit(cluster.data)
-            c1, c2 = self.Cluster.build(cluster.data, k_means, cluster.index)
-
-            # Den Pelleg (2000)
-            bic = (c1.log_likelihood() + c2.log_likelihood()) - cluster.df/2 * np.log(cluster.size)
-
-            # Ishioka (2000)
-            #beta = np.linalg.norm(c1.center - c2.center) / np.sqrt(np.linalg.det(c1.cov) + np.linalg.det(c2.cov))
-            #alpha = 0.5 / stats.norm.cdf(beta)
-            #bic = (cluster.size * np.log(alpha) + c1.log_likelihood() + c2.log_likelihood()) - cluster.df/2 * np.log(cluster.size)
-
-            if bic < cluster.bic():
-                self.__recursively_split([c1, c2])
-            else:
-                self.__clusters.append(cluster)
-
-    class Cluster:
-        """
-        k-means法によって生成されたクラスタに関する情報を持ち、尤度やBICの計算を行うクラス
-        """
-
-        @classmethod
-        def build(cls, X, k_means, index = None):
-            if np.any(index == None):
-                index = np.array(range(0, X.shape[0]))
-            labels = range(0, k_means.get_params()["n_clusters"])
-
-            return tuple(cls(X, index, k_means, label) for label in labels)
-
-        # index: Xの各行におけるサンプルが元データの何行目のものかを示すベクトル
-        def __init__(self, X, index, k_means, label):
-            self.data = X[k_means.labels_ == label]
-            self.index = index[k_means.labels_ == label]
-            self.size = self.data.shape[0]
-            self.df = self.data.shape[1] * (self.data.shape[1] + 3) / 2
-            self.center = k_means.cluster_centers_[label]
-            self.cov = np.cov(self.data.T)
-            # self.cov = 1/(self.data.size - 2) * (np.linalg.norm(self.data - np.expand_dims(np.mean(self.data, axis=1), axis=1)))
-
-        def log_likelihood(self):
-            return sum(stats.multivariate_normal.logpdf(x, self.center, self.cov) for x in self.data)
-
-        def bic(self):
-            return self.log_likelihood() - self.df/2 * np.log(self.size)
 
 def x_means(samples, threshold, seed=None):
     """
--- sources/x-means/generate_samples.py
+++ sources/x-means/generate_samples.py
@@ -12,14 +12,14 @@ from functions import *
 n_features = 2
 n_clusters = 5
 n_samples_per_cluster = 1000
-seed = 1
+seed = np.random.randint(1)
 embiggen_factor = 70
 threshold = 0.1e-9
 
 samples, labels = datasets.make_blobs(
         n_samples = n_samples_per_cluster,
         centers = n_clusters,
-        random_state=seed
+        # random_state=seed
         )
 samples = tf.Variable(samples)
 labels = tf.Variable(labels)
@@ -35,5 +35,5 @@ with tf.Session() as session:
     centroids = session.run(centroids)
     plot_samples(sample_values, save=True, name='before')
     x_means = XMeans(random_state=1).fit(sample_values)
-    plot_clusters(sample_values, x_means.labels_, x_means.cluster_centers_, n_samples_per_cluster, save=True, name='after')
-    plot_clusters(sample_values, label, x_means.cluster_centers_, n_samples_per_cluster, save=True, name='true')
+    plot_clusters(sample_values, x_means.labels_, n_samples_per_cluster, x_means.cluster_centers_, save=True, name='after')
+    plot_clusters(sample_values, label, n_samples_per_cluster, centroids, edit_centroids=False, save=True, name='true')
